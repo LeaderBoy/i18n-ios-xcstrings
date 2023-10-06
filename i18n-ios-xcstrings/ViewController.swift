b60cb9b7857703642dee82c86d05ce18db0ea58b
M1 Macbook <748603349@qq.com>
2023-10-06 09:47:51 +0800

    Initial Commit

---
+++ i18n-ios-xcstrings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i18n-ios-xcstrings
+//
+//  Created by 杨志远 on 2023/10/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
